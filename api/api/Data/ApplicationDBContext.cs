fc216dffa9db62052dac7aa6bb4800132d4f19e3
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-06 15:43:36 +0200

    identity + jwt

--- api/api/Data/ApplicationDBContext.cs
+++ api/api/Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,5 +18,40 @@ namespace api.Data
         }
 
         public DbSet<TicketModel> TicketModels { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+                new IdentityRole
+                {
+                    Name = "L1",
+                    NormalizedName = "L1"
+                },
+                new IdentityRole
+                {
+                    Name = "L2",
+                    NormalizedName = "L2"
+                },
+                new IdentityRole
+                {
+                    Name = "L3",
+                    NormalizedName = "L3"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }

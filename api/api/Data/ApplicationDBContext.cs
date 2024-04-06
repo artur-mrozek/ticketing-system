ed847ec38d9b978006e98d78a2b246f0a6941dd6
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-06 14:54:37 +0200

    identity and database connection

---
+++ api/api/Data/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<TicketModel> TicketModels { get; set; } = null!;
+    }
+}

6a85e8855990666cc37c5fd722755d18995d2268
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-06 15:09:42 +0200

    Ticket Dto

---
+++ api/api/Dtos/Ticket/TicketDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Ticket
+{
+    public class TicketDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string CreatedBy { get; set; } = null!;
+    }
+}

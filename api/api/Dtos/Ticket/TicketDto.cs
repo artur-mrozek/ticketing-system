d085026b1e9712c86dcd4fff4e7c49738280329e
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-07 14:24:34 +0200

    change ticket dto

--- api/api/Dtos/Ticket/TicketDto.cs
+++ api/api/Dtos/Ticket/TicketDto.cs
@@ -7,7 +7,6 @@ namespace api.Dtos.Ticket
 {
     public class TicketDto
     {
-        public int Id { get; set; }
         public string Category { get; set; } = null!;
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;

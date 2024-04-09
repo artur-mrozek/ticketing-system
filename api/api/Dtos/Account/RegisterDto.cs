41f5962249a517fff05fc15368421fe9e2832441
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-09 20:48:26 +0200

    register dto data validation error message correction

--- api/api/Dtos/Account/RegisterDto.cs
+++ api/api/Dtos/Account/RegisterDto.cs
@@ -25,7 +25,7 @@ namespace api.Dtos.Account
         [MaxLength(40, ErrorMessage = "Last name cannot be over 40 characters")]
         public string LastName { get; set; } = null!;
         [Required]
-        [MaxLength(200, ErrorMessage = "First name cannot be over 200 characters")]
+        [MaxLength(200, ErrorMessage = "Address cannot be over 200 characters")]
         public string Address { get; set; } = null!;
         [Required]
         public string Password { get; set; } = null!;

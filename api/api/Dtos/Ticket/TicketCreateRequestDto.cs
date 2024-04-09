7c3379de92fac2cb966a7f2c9abf65677e2f9342
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-09 19:33:23 +0200

    ticket dtos data validation

--- api/api/Dtos/Ticket/TicketCreateRequestDto.cs
+++ api/api/Dtos/Ticket/TicketCreateRequestDto.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace api.Dtos.Ticket
 {
     public class TicketCreateRequestDto
     {
+        [MaxLength(40, ErrorMessage = "Category cannot be over 40 characters")]
         public string Category { get; set; } = null!;
+        [MaxLength(60, ErrorMessage = "Category cannot be over 60 characters")]
         public string Title { get; set; } = null!;
+        [MaxLength(2500, ErrorMessage = "Category cannot be over 2500 characters")]
         public string Description { get; set; } = null!;
     }
 }
--- api/api/Dtos/Ticket/UpdateTicketRequestDto.cs
+++ api/api/Dtos/Ticket/UpdateTicketRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace api.Dtos.Ticket
 {
     public class UpdateTicketRequestDto
     {
+        [MaxLength(40, ErrorMessage = "Category cannot be over 40 characters")]
         public string? Category { get; set; }
+        [MaxLength(60, ErrorMessage = "Category cannot be over 60 characters")]
         public string? Title { get; set; }
+        [MaxLength(2500, ErrorMessage = "Category cannot be over 2500 characters")]
         public string? Description { get; set; }
     }
 }

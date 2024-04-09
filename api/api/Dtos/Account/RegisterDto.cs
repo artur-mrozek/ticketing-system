4ed58a92942d6fcdffcd7c2094d633f53d85ec75
artur-mrozek <artur.mrozek1@gmail.com>
2024-04-09 20:44:19 +0200

    register dto data validation

--- api/api/Dtos/Account/RegisterDto.cs
+++ api/api/Dtos/Account/RegisterDto.cs
@@ -17,10 +17,15 @@ namespace api.Dtos.Account
         [Phone]
         public string PhoneNumber { get; set; } = null!;
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [MaxLength(40, ErrorMessage = "First name cannot be over 40 characters")]
         public string FirstName { get; set; } = null!;
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [MaxLength(40, ErrorMessage = "Last name cannot be over 40 characters")]
         public string LastName { get; set; } = null!;
         [Required]
+        [MaxLength(200, ErrorMessage = "First name cannot be over 200 characters")]
         public string Address { get; set; } = null!;
         [Required]
         public string Password { get; set; } = null!;
